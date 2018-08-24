86a8e70ac98286ccd6413bb3f46c1f5ea50b59eb
RoroftheCrowd <32059576+RoroftheCrowd@users.noreply.github.com>
2018-08-24 21:24:50 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ContactList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAddContact = new System.Windows.Forms.Button();
+            this.lstContacts = new System.Windows.Forms.ListBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(167, 57);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(163, 23);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(167, 102);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(163, 23);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(167, 150);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(163, 23);
+            this.txtPhone.TabIndex = 5;
+            // 
+            // btnAddContact
+            // 
+            this.btnAddContact.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAddContact.Location = new System.Drawing.Point(74, 195);
+            this.btnAddContact.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddContact.Name = "btnAddContact";
+            this.btnAddContact.Size = new System.Drawing.Size(133, 34);
+            this.btnAddContact.TabIndex = 6;
+            this.btnAddContact.Text = "Add Contact";
+            this.btnAddContact.UseVisualStyleBackColor = false;
+            this.btnAddContact.Click += new System.EventHandler(this.btnAddContact_Click);
+            // 
+            // lstContacts
+            // 
+            this.lstContacts.BackColor = System.Drawing.SystemColors.Window;
+            this.lstContacts.FormattingEnabled = true;
+            this.lstContacts.ItemHeight = 16;
+            this.lstContacts.Location = new System.Drawing.Point(412, 15);
+            this.lstContacts.Margin = new System.Windows.Forms.Padding(4);
+            this.lstContacts.Name = "lstContacts";
+            this.lstContacts.Size = new System.Drawing.Size(224, 228);
+            this.lstContacts.TabIndex = 7;
+            // 
+            // btnSort
+            // 
+            this.btnSort.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSort.Location = new System.Drawing.Point(225, 195);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(133, 34);
+            this.btnSort.TabIndex = 8;
+            this.btnSort.Text = "Sort List";
+            this.btnSort.UseVisualStyleBackColor = false;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label4.Location = new System.Drawing.Point(26, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(370, 38);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Nightclub Guest List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(678, 295);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.lstContacts);
+            this.Controls.Add(this.btnAddContact);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAddContact;
+        private System.Windows.Forms.ListBox lstContacts;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
